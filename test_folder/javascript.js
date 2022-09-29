4f799751a67fe632ed87baa137bdda35ff0652e9
JHoang <JHoang6900@gmail.com>
2022-09-29 05:56:02 -0600

    implement generate password function

---
+++ test_folder/javascript.js
@@ -0,0 +1,62 @@
+var password_lowercase = confirm('Include lowercase characters?');
+var password_uppercase = confirm('Include uppercase characters?');
+var password_numeric = confirm('Include numeric characters?');
+var password_special_chars = confirm('Include special characters?');
+  
+  if (password_lowercase){
+    password_lowercase = "abcdefghijklmnopqrstuvwxyz";
+  } else {
+    var password_lowercase = "";
+  }
+
+  if(password_uppercase){
+    var password_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else {
+    var password_uppercase = "";
+  }
+
+  if(password_numeric){
+    var password_numeric = "0123456789"
+  } else {
+    var password_numeric = "";
+  }
+
+  if(password_special_chars){
+    var password_special_chars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  } else {
+    var password_special_chars = "";
+  }
+
+//   console.log('password_length :>> '+ password_length + ' characters.')
+  console.log('password_lowercase :>>', password_lowercase);
+  console.log('password_uppercase :>>', password_uppercase);
+  console.log('password_numeric :>>', password_numeric);
+  console.log('password_special_chars :>>', password_special_chars);
+
+
+var password_length = prompt('How many characters will your password contain?');
+console.log('password_length is ~> '+ password_length + ' characters.');
+console.log('adding +1 to password_length...', password_length++);
+console.log('password_length +1 is ~> ', password_length);
+
+var allowed_characters = password_lowercase + password_uppercase + password_numeric + password_special_chars;
+console.log('allowed_characters ~>', allowed_characters);
+
+var finalPass = ''
+
+for ( var i = 0; i < password_length; i++ ) {
+
+    var random_number = Math.floor(Math.random() * allowed_characters.length);
+    console.log('picking a random number between 0 and '+ allowed_characters.length + '.' , random_number);
+    
+    
+    var index = random_number;
+    console.log(`The character at index ${index} is ${allowed_characters.charAt(index)}`);
+
+    finalPass += allowed_characters.charAt(index);
+ }
+ console.log(finalPass);
+
+
+ // min/max 8-126 characters for password_length
+ // 1 of each of the 4 character types 
---
+++ test_folder/random_number.js
@@ -0,0 +1,50 @@
+const allowed_characters = 'Life, the universe, stars and everything. Answer:';
+
+console.log(`${allowed_characters} ${allowed_characters.length}`);
+// expected output: "Life, the universe and everything. Answer: 42"
+
+
+
+// the most perfect code almost
+// var password_length = prompt('what is your password_length number?')
+// console.log('password_length is ~>', password_length);
+// console.log('adding +1 to password_length...', password_length++);
+// console.log('password_length +1 is ~> ', password_length);
+
+// var random_number = Math.floor(Math.random() * password_length);
+// console.log('picking a random number between 0 and '+ password_length + '.' , random_number);
+     
+//   var allowed_characters = 'abcedfghijklmnopqrstuvwxyzABCEDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+//   var index = random_number;
+  
+//   console.log(`The character at index ${index} is ${allowed_characters.charAt(index)}`);
+
+//   var finalPass = allowed_characters.charAt(index)
+
+
+
+//   for ( var i = 0; i < password_length; i++ ) {
+//     finalPass += allowed_characters.charAt(Math.floor(Math.random() * 
+// password_length));
+//  }
+//  console.log(finalPass);
+ 
+// expected output: "The character at index 4 is q"
+
+
+
+
+
+
+
+// var a = 1;
+// console.log(a++);    // 1
+// console.log(a++);    // 2
+// console.log(a);      // 3
+
+
+// allowed_characters = randomly index the string of allowed characters. 
+
+// add 26 to password_length 
+// limit to 93 characters. 
+// maybe two different variables for password_length and index number 
